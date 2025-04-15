e2b9dd9fcc9d5d1203445bb34557946e08fbadab
Iván Recasens <irecasensl@gmail.com>
2025-04-15 17:56:29 +0200

    Avoid m+n problem

--- src/main/java/com/ivanrl/yaet/yaetApp/expenses/ExpenseRepository.java
+++ src/main/java/com/ivanrl/yaet/yaetApp/expenses/ExpenseRepository.java
@@ -1,16 +1,31 @@
 package com.ivanrl.yaet.yaetApp.expenses;
 
-import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.util.List;
 
 public interface ExpenseRepository extends JpaRepository<ExpensePO, Integer> {
 
-    Sort defaultSorting = Sort.by(Sort.Direction.ASC, "date");
+    // Ordering is not strictly needed, I'm just getting it DB side
+    @Query("""
+            FROM expenses e
+            JOIN FETCH e.category c
+            WHERE :from <= e.date
+            AND e.date <= :to
+            ORDER BY e.date ASC
+            """)
+    List<ExpensePO> findAllByDateBetween(@Param("from") LocalDate from,
+                                         @Param("to") LocalDate to);
 
-    List<ExpensePO> findAllByDateBetween(LocalDate from, LocalDate to, Sort defaultSorting);
-
-    List<ExpensePO> findTop10ByOrderByDateDesc();
+    @Query("""
+            FROM expenses e
+            JOIN FETCH e.category c
+            ORDER BY e.date DESC
+            """)
+    Page<ExpensePO> findLastExpenses(Pageable pagination);
 }
