83891e55efaeafddca653a78582a660f3d606ad0
Iván Recasens <irecasensl@gmail.com>
2025-09-20 21:56:14 +0200

    Update main controller to accept common initial routes

--- src/main/java/com/ivanrl/yaet/yaetApp/MainController.java
+++ src/main/java/com/ivanrl/yaet/yaetApp/MainController.java
@@ -1,17 +1,13 @@
 package com.ivanrl.yaet.yaetApp;
 
-import com.ivanrl.yaet.yaetApp.expenses.ExpenseRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/home")
 public class MainController {
 
-    @GetMapping
+    @GetMapping(value = {"", "/", "/home"})
     public String home(Model model) {
 
         return "home";
