4612d1fc4169eb9ba42cd7dd11d00008866792a8
Iván Recasens <irecasensl@gmail.com>
2025-10-04 11:43:56 +0200

    Create configuration for in memory H2 DB for tests

---
+++ src/test/java/com/ivanrl/yaet/H2TestConfig.java
@@ -0,0 +1,26 @@
+package com.ivanrl.yaet;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableJpaRepositories(basePackages = "com.ivanrl.yaet.persistence")
+@EnableTransactionManagement
+public class H2TestConfig {
+
+    @Bean
+    public DataSource h2InMemoryDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.h2.Driver");
+        dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
+        dataSource.setUsername("sa");
+        dataSource.setPassword("sa");
+
+        return dataSource;
+    }
+}
--- src/test/java/com/ivanrl/yaet/YaetAppApplicationTests.java
+++ src/test/java/com/ivanrl/yaet/YaetAppApplicationTests.java
@@ -3,7 +3,7 @@ package com.ivanrl.yaet;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = {H2TestConfig.class, YaetAppApplication.class})
 class YaetAppApplicationTests {
 
 	@Test
