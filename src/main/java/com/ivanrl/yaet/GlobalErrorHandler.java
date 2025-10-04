7bceb3cb4622f0d6c734da2b10437990620f9d32
Iván Recasens <irecasensl@gmail.com>
2025-10-04 15:06:28 +0200

    Customize the logging of globally handled exceptions

--- src/main/java/com/ivanrl/yaet/GlobalErrorHandler.java
+++ src/main/java/com/ivanrl/yaet/GlobalErrorHandler.java
@@ -1,24 +1,48 @@
 package com.ivanrl.yaet;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 @ControllerAdvice
+@Slf4j
 public class GlobalErrorHandler {
+
+
+    /**
+     * Logs the exception details with level ERROR
+     * @param e the exception being handled
+     * @param problem the {@link ProblemDetail} object sent back as the response
+     */
+    private static void logHandledException(Exception e,
+                                            ProblemDetail problem) {
+        log.error("{} handled\n\tResponse: {}\n\n{}",
+                  e.getClass().getSimpleName(),
+                  problem,
+                  Arrays.stream(e.getStackTrace())
+                        .map(StackTraceElement::toString)
+                        .collect(Collectors.joining("\n\t")));
+    }
 
     @ExceptionHandler(BadRequestException.class)
     public ProblemDetail handleBadRequest(BadRequestException bre) {
         var problem = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, bre.getMessage());
         problem.setTitle(bre.getTitle());
+        logHandledException(bre, problem);
         return problem;
     }
 
     @ExceptionHandler(Exception.class)
     public ProblemDetail handleException(Exception e) {
-        var problem = ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, "Something went wrong. Please, try again.");
+        var problem = ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR,
+                                                       "Something went wrong. Please, try again.");
         problem.setTitle("Oops!");
+        logHandledException(e, problem);
         return problem;
     }
 }
