bd1d567b0a1650fba42fc7eb4852633b8a342357
Iván Recasens <irecasensl@gmail.com>
2025-10-10 21:45:29 +0200

    Add more validations to UpdateExpenseRequestTO

--- src/main/java/com/ivanrl/yaet/web/UpdateExpenseRequestTO.java
+++ src/main/java/com/ivanrl/yaet/web/UpdateExpenseRequestTO.java
@@ -2,22 +2,25 @@ package com.ivanrl.yaet.web;
 
 import com.ivanrl.yaet.domain.expense.ExpenseWithCategoryDO;
 import com.ivanrl.yaet.domain.expense.UpdateExpenseRequest;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 // TODO Consider if a specific annotation should be defined for web objects
-record UpdateExpenseRequestTO(int id,
-                              int categoryId,
+record UpdateExpenseRequestTO(@NotNull(message = "There is an issue when identifying this expense")
+                              Integer id,
+                              @NotNull(message = "Could not get category information")
+                              Integer categoryId,
                               @NotEmpty(message = "Payee must not be empty")
                               @Size(max = 50, message = "Payee cannot exceed 50 characters")
                               String payee,
                               @NotNull(message = "Date must not be empty")
+                              @PastOrPresent(message = "Date cannot be in the future")
                               LocalDate date,
                               @NotNull(message = "Amount must not be empty")
+                              @PositiveOrZero(message = "Amount should be a value bigger than 0")
+                              @DecimalMax(value = "9999.99", message = "Amount must be less than 10000")
                               BigDecimal amount,
                               @Size(max = 255, message = "Comment must not exceed 255 characters")
                               String comment) {
