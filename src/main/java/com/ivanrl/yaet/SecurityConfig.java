275614146f4eee49878941ad57a2af8f985bb08c
Iván Recasens <irecasensl@gmail.com>
2025-11-19 21:35:03 +0100

    Enable automatic re-login

--- src/main/java/com/ivanrl/yaet/SecurityConfig.java
+++ src/main/java/com/ivanrl/yaet/SecurityConfig.java
@@ -3,9 +3,12 @@ package com.ivanrl.yaet;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -22,6 +25,10 @@ public class SecurityConfig {
             )
 
             .oauth2Login(o -> o.successHandler(oAuth2LoginSuccessHandler))
+            .exceptionHandling(ex -> ex.defaultAuthenticationEntryPointFor(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED),
+                                                                           new AntPathRequestMatcher("/**")
+                    )
+            )
         ;
 
         return http.build();
